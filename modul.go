3c7a41419f62a61ca2977698384598dbc102013a
karsinogenic <muhmmadnaufl@gmail.com>
2022-03-12 12:24:08 +0700

    Update modul.go

--- modul.go
+++ modul.go
@@ -4,13 +4,13 @@ package modul
 
 import "fmt"
 
-// type item struct {
-// 	nama     string
-// 	stock    int
-// 	harga    int
-// 	nBelanja int
-// 	nTotal   int
-// }
+type item struct {
+	nama     string
+	stock    int
+	harga    int
+	nBelanja int
+	nTotal   int
+}
 
 func Kasir(uang, bayar int) int {
 	var kembali int
@@ -45,25 +45,25 @@ func Coba1(coba int) {
 
 // var minyak = item{"minyak", 10, 30000, 0, 0}
 
-// func (minyak item) Belanja1() (int, int) {
-// 	var banyak int
-// 	var ntotal int
-// ulang1:
-// 	fmt.Printf("Anda memilih minyak, berapa banyak yang ingin anda beli : ")
-// 	//fmt.Printf("1.%s stock:%d harga:%s || 2.%s stock:%d harga:%s\n//////////////////////////////////////////////////////\n", minyak.nama, minyak.stock, minyak.harga, terigu.nama, terigu.stock, terigu.harga)
-// 	//fmt.Print("Pilihan anda (1-2) : ")
-// 	fmt.Scanln(&banyak)
-// 	if banyak > minyak.stock {
-// 		fmt.Println("Stock tidak mencukupi, jumlah stock : ", minyak.stock)
-// 		goto ulang1
-// 	} else {
-// 		minyak.nBelanja = minyak.nBelanja + banyak
-// 		fmt.Println("Total minyak yang anda beli : ", minyak.nBelanja)
-// 		minyak.nTotal = minyak.nTotal + (minyak.nBelanja * minyak.harga)
-// 		ntotal = minyak.nTotal
-// 		fmt.Println("Total harga minyak yang anda beli : ", minyak.nTotal)
-// 		minyak.stock = minyak.stock - minyak.nBelanja
-// 		fmt.Println("Sisa stock: ", minyak.stock)
-// 	}
-// 	return banyak, ntotal
-// }
+func (minyak item) Belanja1() (int, int) {
+	var banyak int
+	var ntotal int
+ulang1:
+	fmt.Printf("Anda memilih minyak, berapa banyak yang ingin anda beli : ")
+	//fmt.Printf("1.%s stock:%d harga:%s || 2.%s stock:%d harga:%s\n//////////////////////////////////////////////////////\n", minyak.nama, minyak.stock, minyak.harga, terigu.nama, terigu.stock, terigu.harga)
+	//fmt.Print("Pilihan anda (1-2) : ")
+	fmt.Scanln(&banyak)
+	if banyak > minyak.stock {
+		fmt.Println("Stock tidak mencukupi, jumlah stock : ", minyak.stock)
+		goto ulang1
+	} else {
+		minyak.nBelanja = minyak.nBelanja + banyak
+		fmt.Println("Total minyak yang anda beli : ", minyak.nBelanja)
+		minyak.nTotal = minyak.nTotal + (minyak.nBelanja * minyak.harga)
+		ntotal = minyak.nTotal
+		fmt.Println("Total harga minyak yang anda beli : ", minyak.nTotal)
+		minyak.stock = minyak.stock - minyak.nBelanja
+		fmt.Println("Sisa stock: ", minyak.stock)
+	}
+	return banyak, ntotal
+}
