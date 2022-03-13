3fce18c9dd77985352ebbced6d624fa49c5cdb31
karsinogenic <muhmmadnaufl@gmail.com>
2022-03-13 07:08:06 +0700

    baru

--- modul.go
+++ modul.go
@@ -41,25 +41,25 @@ ulang:
 
 // var minyak = item{"minyak", 10, 30000, 0, 0}
 
-func (minyak Item) Belanja1() (int, int) {
+func (item Item) Belanja1() (int, int) {
 	var banyak int
 	var Ntotal int
 ulang1:
-	fmt.Printf("Anda memilih minyak, berapa banyak yang ingin anda beli : ")
+	fmt.Printf("Anda memilih %s, berapa banyak yang ingin anda beli : ", item.Nama)
 	//fmt.Printf("1.%s Stock:%d Harga:%s || 2.%s Stock:%d Harga:%s\n//////////////////////////////////////////////////////\n", minyak.Nama, minyak.Stock, minyak.Harga, terigu.Nama, terigu.Stock, terigu.Harga)
 	//fmt.Print("Pilihan anda (1-2) : ")
 	fmt.Scanln(&banyak)
-	if banyak > minyak.Stock {
-		fmt.Println("Stock tidak mencukupi, jumlah Stock : ", minyak.Stock)
+	if banyak > item.Stock {
+		fmt.Println("Stock tidak mencukupi, jumlah Stock : ", item.Stock)
 		goto ulang1
 	} else {
-		minyak.Nbelanja = minyak.Nbelanja + banyak
-		fmt.Println("Total minyak yang anda beli : ", minyak.Nbelanja)
-		minyak.Ntotal = minyak.Ntotal + (minyak.Nbelanja * minyak.Harga)
-		Ntotal = minyak.Ntotal
-		fmt.Println("Total Harga minyak yang anda beli : ", minyak.Ntotal)
-		minyak.Stock = minyak.Stock - minyak.Nbelanja
-		fmt.Println("Sisa Stock: ", minyak.Stock)
+		item.Nbelanja = item.Nbelanja + banyak
+		fmt.Println("Total minyak yang anda beli : ", item.Nbelanja)
+		item.Ntotal = item.Ntotal + (item.Nbelanja * item.Harga)
+		Ntotal = item.Ntotal
+		fmt.Println("Total Harga minyak yang anda beli : ", item.Ntotal)
+		item.Stock = item.Stock - item.Nbelanja
+		fmt.Println("Sisa Stock: ", item.Stock)
 	}
 	return banyak, Ntotal
 }
